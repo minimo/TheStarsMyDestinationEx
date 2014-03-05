0c2e3823382fa54a0e348609dc3a4bb303794091
minimo <minimo.mobile@gmail.com>
2014-03-05 10:30:53 +0900

    add TutorialScene.js

---
+++ src/scene/TutorialScene.js
@@ -0,0 +1,19 @@
+/*
+ *  TheStarsMyDestination tmlib.js version
+ *  TutorialScene.js
+ *  2014/03/05
+ *  @auther minimo  
+ *  This Program is MIT license.
+ */
+ 
+//チュートリアルシーン
+tm.define("tiger.TutorialScene", {
+    superClass: tm.app.Scene,
+
+    init: function() {
+        this.superInit();
+        app.background = "rgba(0, 0, 0, 0)";
+    },
+    update: function() {
+    },
+});
