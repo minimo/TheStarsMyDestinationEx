cd1d83b64e7667545104785825dba6ea66dfcf67
minimo <minimo.mobile@gmail.com>
2014-03-06 19:54:47 +0900

    sliderbar

--- src/UserInterface.js
+++ src/UserInterface.js
@@ -7,15 +7,27 @@
  *
  */
 
-//マップ管理クラス
+//スライダーバー
 tm.define("tiger.Sliderbar", {
-    superClass: tm.app.Object2D,
+    superClass: "tm.display.CanvasElement",
 
-    init: function(scene) {
+    //現在値
+    value: 50,
+
+    //最小値
+    min: 0,
+
+    //最大値
+    max: 100,
+
+    init: function(width, height) {
         this.superInit();
+        this.width = width || 100;
+        this.height = height || 16;
     },
 
-    update: function() {
+    draw: function(canvas) {
+        canvas.fillStyle = "rgba(0, 64, 255, 0.8)";
+        canvas.fillRoundRect(0, this.height/3, this.width, this.height/3, this.height/6);
     },
-    
 });
